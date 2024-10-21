4ad428d0d395b3ec470448cb25259b71bd8baaaa
Felipe William Silva Paulino <54473148+Lukexll@users.noreply.github.com>
2024-10-21 09:51:27 -0300

    Update index.js

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -26,7 +26,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch('http://68.183.117.75:5152/loginaccount', {
+      const response = await fetch('https://68.183.117.75:5152/loginaccount', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
